099ba12278296a35df3d0665aad385cfaa8ad76d
Reid Roberts <reidroberts24@gmail.com>
2023-10-24 21:54:57 -0500

    first solution

---
+++ reorder_list.py
@@ -0,0 +1,18 @@
+import math
+head = [1, 2, 3, 4, 5, 6]
+
+def reorder_list(head):
+    mid = math.ceil(len(head)/2)
+    first_half = head[:mid]
+    second_half = head[mid:]
+    res = []
+
+    for i in range(len(first_half)):
+    
+        res.append(first_half[i])
+        if second_half:
+            res.append(second_half[-1])
+            second_half.pop()
+    return res
+
+print(reorder_list(head))
