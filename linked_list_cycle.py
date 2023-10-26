14406a13ccbb98f6c1dd8d59bdec4a1a3e6afaf4
Reid Roberts <reidroberts24@gmail.com>
2023-10-26 18:27:04 -0500

    commit

---
+++ linked_list_cycle.py
@@ -0,0 +1,13 @@
+head = [3,2,0,-4]
+pos = 1
+
+
+def has_cycle():
+
+    hashSet = set()
+    while head:
+        if head in hashSet:
+            return True
+        hashSet.add(head)
+        head = head.next
+    return False
