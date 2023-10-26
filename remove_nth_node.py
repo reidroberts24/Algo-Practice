0a623da0c5e9b53fb9a6b0b8f9e0b4a00abcac36
Reid Roberts <reidroberts24@gmail.com>
2023-10-26 17:32:37 -0500

    remove_nth_node

---
+++ remove_nth_node.py
@@ -0,0 +1,12 @@
+'''Given the head of a linked list, remove the nth node from the end of the list and return its head.
+'''
+
+head = [1,2,3,4,5] 
+n = 2
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
