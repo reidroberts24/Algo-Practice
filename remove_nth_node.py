80892a49e0f9073380acc39f56c1c2b05db8e2d4
Reid Roberts <reidroberts24@gmail.com>
2023-10-26 18:10:58 -0500

    solved

--- remove_nth_node.py
+++ remove_nth_node.py
@@ -1,23 +1,28 @@
 '''Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+
+leetcode solution
 '''
 
 head = [1,2,3,4,5] 
 n = 2
 
-class Node:
-    def __init__(self, value):
+class ListNode:
+    def __init__(self, value, next=None):
         self.value = value
         self.next = None
 
 
 def remove_nth_from_end(head):
+    dummy = ListNode(0, head)
+    left = dummy
+    right = head
+    while n > 0 and right:
+        right = right.next
+        n -= 1
+
+    while right:
+        left = left.next
+        right = right.next
     
-    traveler = head
-    prev = None
-
-    while head and head.next:
-        prev = head
-        temp = head.next.next
-
-        head = head.next
-        head.next = prev
+    left.next = left.next.next
